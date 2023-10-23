fab5411e02f797c3d85b0a731079e94b4cd771b0
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-10-23 15:51:40 -0300

    translate

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 #------------------------------------------------- Libraries ------------------------------------------------- 
 import pandas as pd
 import numpy as np
-from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_validate
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+
 #------------------------------------------------- Read Dataset ------------------------------------------------- 
+
 data = pd.read_csv("datasets_9768_13874_HR_comma_sep.csv")
-print(data)
+
 
 #------------------------------------------------- Info Dataset ------------------------------------------------- 
-
-data.info()
-data.head()
-data.isnull().sum()
-data.shape
-# Our data is pretty clean, with no missing values, so let's move futher and see how many\
-#employees work in the organization
-
+print(data.head()) 
+data.info() 
+data.isnull().sum()  
+print("Formato do dataset:", data.shape) # result (14999, 19)
+data.columns
 #------------------------------------------------- Data Cleaning ------------------------------------------------- 
 
 # rename column name from "sales" to "departament"
 data = data.rename(columns= {"sales":"department"})
-data["department"].unique()
+print("Departamentos únicos:", data["department"].unique())
 
 #------------------------------------------------- Data Analysis ------------------------------------------------- 
 
@@ -50,7 +52,7 @@ for var in cat_vars:
 # Now the actual variables nees to be removed after the dummy variable have been created. Column names\
 # after creating dummy variables for categorical variables
 data.drop(data.columns[[8,9]],axis=1,inplace=True)
-data.columns.values
+print("Nomes das colunas após a criação das variáveis dummy:", data.columns.values)
 
 #------------------------------------------------------------------------------------------------------ 
 
@@ -59,30 +61,171 @@ data_vars=data.columns.values.tolist()
 y=["left"]
 x=[i for i in data_vars if i not in y]
 
+#------------------------------------------------------------------------------------------------------ 
+
+# Calculate percentage of employess who "
+porcentagem_saída = (data["left"].sum() / data.shape[0])*100 #result = 23.80825388359224
 
 #------------------------------------------------- Model Predict ------------------------------------------------- 
-# Features Selection for employeed turnover prediction
 
-# Let's use the feature selection method to decid which variables are the best option that can predict\
-# employeed turnover with great accuracy. There are a total of 18 columns in x, and now "left" see how\
-# we can select about 10 from them:
+cols=['satisfaction_level', 'last_evaluation', 'time_spend_company', 'Work_accident', 'promotion_last_5years','department_RandD', 'department_hr', 'department_management', 'salary_high', 'salary_low'] 
+X=data[cols]
+y=data['left']
+
+# Splitting data into train an test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#  Creating and training logistic regression model
 model = LogisticRegression()
-rfe = RFE(estimator=model, n_features_to_select=10)
-rfe = rfe.fit(data[x], data[y])
-print(rfe.support_)
-print(rfe.ranking_)
+model.fit(X_train, y_train)
+
+#  Prediction on test set
+y_pred = model.predict(X_test)
+
+# Model evaluation
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+# View results
+print("Acurácia do modelo: {:.2f}%".format(accuracy * 100))
+print("Matriz de confusão")
+print(confusion)
+print("\nRelatório de Classificação")
+print(report)
 
 #------------------------------------------------------------------------------------------------------ 
 
-#You can see that or feature selection chose the 10 variables for us, which are marked True in the \ 
-#support_ array and marked with a choice “1” in the ranking_array. Now lets have a look at these columns:
+# The performance of our model is not good because the data set is unbalanced it by generating synthetic data
+cols=['satisfaction_level', 'last_evaluation', 'time_spend_company', 'Work_accident', 'promotion_last_5years','department_RandD', 'department_hr', 'department_management', 'salary_high', 'salary_low'] 
+X=data[cols]
+y=data['left']
 
-cols=['satisfaction_level', 'last_evaluation', 'time_spend_company', 'Work_accident', 'promotion_last_5years', 
-      'department_RandD', 'department_hr', 'department_management', 'salary_high', 'salary_low']
+# Splitting data into train an test sets
 
-x = data[cols]
-y = ['left']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+smote = SMOTE(sampling_strategy="auto")
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+#  Creating and training logistic regression model with rebalanced data
+
+# Predict on test set
+y_pred = model.predict(X_test)
+
+# Model Evaluation
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+# View results
+print("Acurácia do modelo com dados de teste: {:.2f}%".format(accuracy * 100))
+print("Matriz de Confusão:")
+print(confusion)
+print("Relatório de Classificação:")
+print(report)
+
+# Our Model improved in detecting class 1(employees leaving) compared to the unbalanced model.
+# The recall for classa 1 also increased to 78% after balancing the classes while in the previous one it was 0.24
+
+#------------------------------------------------------------------------------------------------------ 
+# Now we will adjust the hyperparameters to try to improve the performance of the algorithm
+
+# Hyperparameter grid for search
+param_grid = {
+    'C': [0.001, 0.01, 0.1, 1, 10],
+    'penalty': ['l1', 'l2'],
+    'solver': ['liblinear', 'lbfgs'],
+    'max_iter': [100, 200, 300]
+}
+
+# Create a goal to find the best hyperparameters
+grid_search = GridSearchCV(LogisticRegression(), param_grid, cv=5, scoring="accuracy" )
+
+# Train the model with the best combination of hyperparameters
+grid_search.fit(X_train_resampled, y_train_resampled)
+
+# Get the models with the best hyperparameters
+best_model = grid_search.best_estimator_
+
+# Predict Model
+y_pred= best_model.predict(X_test)
+
+# Model evaluation with test data
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+# View results
+print("Acurácia do modelo com dados de teste: {:.2f}%".format(accuracy * 100))
+print("Matriz de Confusão:")
+print(confusion)
+print("Relatório de Classificação:")
+print(report)
+
+# The recall for claa 1 ( employeed leaving)improved even futher, reaching 80%. This means the model is better identifying employees\
+# Who are likely to leave. Accuracy
+
 
 #------------------------------------------------------------------------------------------------------ 
 
-# Logistic Regression Model to Predict Employee Turnover
+# Rando forest
+cols=['satisfaction_level', 'last_evaluation', 'time_spend_company', 'Work_accident', 'promotion_last_5years','department_RandD', 'department_hr', 'department_management', 'salary_high', 'salary_low'] 
+X=data[cols]
+y=data['left']
+
+# Divisão dos dados em conjunto de train e test
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Criar um modelo
+rf_model = RandomForestClassifier(n_estimators=100, max_depth=10,random_state=42)
+
+# Ajuste do modelo
+rf_model.fit(X_train, y_train)
+
+# Previsão com o modelo de teste
+y_pred = rf_model.predict(X_test)
+
+# Avaliação do modelo com os dados de teste
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+# Exibir os resultados
+print("Acurácia do modelo com dados de teste: {:.2f}%".format(accuracy * 100))
+print("Matriz de Confusão:")
+print(confusion)
+print("Relatório de Classificação:")
+print(report)
+
+
+#------------------------------------------------------------------------------------------------------ 
+# Obter a importância das variáveis a partir do modelo Random Forest
+feature_importance = rf_model.feature_importances_
+
+# Normalizar as importâncias para que a soma seja igual a 100%
+total_importance = sum(feature_importance)
+normalized_importance = [imp / total_importance * 100 for imp in feature_importance]
+
+# Criar um dicionário que mapeia o nome do departamento à sua importância normalizada
+department_importance = dict(zip(cols, normalized_importance))
+
+# Classificar os departamentos com base em sua importância
+sorted_departments = sorted(department_importance, key=department_importance.get, reverse=True)
+
+# Exibir os departamentos mais influentes na rotatividade em porcentagem
+print("Departamentos mais influentes na rotatividade (em porcentagem):")
+for department in sorted_departments:
+    print(f"{department}: {department_importance[department]:.2f}%")
+
+
+
+
+
+
+
+
+
+
+
+
